09bae11b03cfe8e647ff63e8d8dcf99e762412ec
Michael J. Miller <mikem.exe@gmail.com>
2020-04-29 13:34:17 -0600

    cleanup

--- 3dSbsToYouTube.zsh
+++ 3dSbsToYouTube.zsh
@@ -1,10 +1,7 @@
 #!/bin/zsh
 for filepath in $*; do
-#   basepath=${filepath%\.*}
-#   extension=${filepath##*\.}
   base=${filepath:h}
   filename=${filepath:t}
   outfile=${base}/SBS_${filename}
   ffmpeg -i ${filepath} -c copy -metadata:s:v:0 stereo_mode=2 "${outfile}"
-#   echo $filepath;
 done
